9aa9556a154582bf731aa7890077a26d340d881e
AdilNaitDaoud <adil.naitdaoud@itnewvision.com>
2020-01-27 16:22:26 +0000

    Httpclient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {interval, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'monProjetAng';
+export class AppComponent implements OnInit, OnDestroy {
+    title = 'FirstApp';
+
+    seconds: number;
+    counterSubscription: Subscription;
+
+constructor() {
+}
+
+
+  ngOnInit() {
+  const counter = interval(1000);
+  this.counterSubscription = counter.subscribe(
+    (value: number) => {
+      this.seconds = value;
+    }
+  );
+  }
+
+  ngOnDestroy() {
+  this.counterSubscription.unsubscribe();
+  }
+
+
 }
